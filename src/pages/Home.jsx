115c59769571bfa5154e6a200174dbcb08091388
ddos <59387969+druvdub@users.noreply.github.com>
2022-09-09 23:44:07 +0530

    Portfolio struct: Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Home = () => {
+  const [isToggled, setIsToggled] = useState(false);
+  return (
+    <>
+      {/* <ToggleNavBar /> */}
+      {/* <NavBar /> */}
+      {/* <Hero /> */}
+    </>
+  );
+};
+
+export default Home;
